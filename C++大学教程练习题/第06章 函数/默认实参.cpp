0473c81a457558957b576df90378f164ab4bf717
ZJisme <949945329@qq.com>
2020-12-20 17:58:43 +0800

    Update 默认实参.cpp

--- C++大学教程练习题/第06章 函数/默认实参.cpp
+++ C++大学教程练习题/第06章 函数/默认实参.cpp
@@ -1,13 +1,20 @@
 #include <bits/stdc++.h>
 using namespace std;
 
-void f(int a = 1, int b, int c = 3)
-{ // 默认实参.cpp(5): error C2548: “f”: 缺少形参 2 的默认实参
-    cout << a * b * c << endl;
-}
+// void f(int a = 1, int b, int c = 3)
+// { // 默认实参.cpp(5): error C2548: “f”: 缺少形参 2 的默认实参
+//     cout << a * b * c << endl;
+// }
+
+void f(int a, int b = 3, int c = 10);
 
 int main()
 {
-    // f(1, 3);
+    f(3);
     return 0;
+}
+
+void f(int a, int b, int c)
+{
+    cout << a * b * c << endl;
 }
