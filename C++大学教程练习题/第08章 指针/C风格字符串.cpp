1738c106306733d9fb69d0a337babafe8b80840d
ZJisme <949945329@qq.com>
2020-12-20 22:21:40 +0800

    Create C风格字符串.cpp

---
+++ C++大学教程练习题/第08章 指针/C风格字符串.cpp
@@ -0,0 +1,12 @@
+#include<bits/stdc++.h>
+using namespace std;
+int main()
+{
+    char s[]="TEST";
+    cin>>setw(3);
+    // cin.getline(s,100);
+    cin>>s;
+    cout<<s<<endl;
+    cout<<"return 0;"<<endl;
+    return 0;
+}
