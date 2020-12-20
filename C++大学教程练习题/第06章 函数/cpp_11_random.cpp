2c1de17f218f303c0e39c423baa3f8262772846c
ZJisme <949945329@qq.com>
2020-12-20 19:15:30 +0800

    Update cpp_11_random.cpp

--- C++大学教程练习题/第06章 函数/cpp_11_random.cpp
+++ C++大学教程练习题/第06章 函数/cpp_11_random.cpp
@@ -7,11 +7,11 @@ int main()
 {
     default_random_engine engine(static_cast<unsigned int>(time(0)));
 
-    uniform_int_distribution<long> randomInt(-101, 6);  // <>表名其为类模板
+    uniform_int_distribution<long> randomInt(-101, 6); // <>表名其为类模板
     cout << randomInt(engine) << endl;
 
     uniform_real_distribution<double> randomReal(-10000, 100);
     cout << randomReal(engine) << endl;
+    
     return 0;
 }
- 
