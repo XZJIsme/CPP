492f393bdd399e7fa6adf3c12a402ed1827f045c
ZJisme <949945329@qq.com>
2020-12-20 18:13:01 +0800

    Create 鍑芥暟妯℃澘.cpp

---
+++ C++大学教程练习题/第06章 函数/函数模板.cpp
@@ -0,0 +1,19 @@
+#include <iostream>
+using namespace std;
+
+template <class T> // template <typename T>
+T maximum(T v1, T v2, T v3)
+{
+    T temp = v1;
+    if (v2 > v1)
+        temp = v2;
+    if (v3 > v1)
+        temp = v3;
+    return v3;
+}
+
+int main()
+{
+    cout << maximum(1, 2, 3) << endl;
+    return 0;
+}
