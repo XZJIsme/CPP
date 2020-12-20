6f3b5d3da9518ecb57f2ab3eafd3c68bee9bd993
ZJisme <949945329@qq.com>
2020-12-20 15:49:58 +0800

    Update tempCodeRunnerFile.cpp

--- tempCodeRunnerFile.cpp
+++ tempCodeRunnerFile.cpp
@@ -1,1 +1,27 @@
-cin >> grade
+#include <iostream>
+
+using namespace std;
+
+class A
+{
+    int a;
+};
+
+class B
+{
+    int b;
+};
+
+class C : public A
+{
+    int c;
+};
+
+int main()
+{
+    B *b = new B;
+    C *c = new C;
+    A *pA2 = static_cast<A *>(c);
+
+    return 0;
+}
