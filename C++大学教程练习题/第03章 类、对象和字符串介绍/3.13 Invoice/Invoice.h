1b9c1a780f8dab54f9259a0ba6d8432d68920037
ZJisme <949945329@qq.com>
2020-12-22 00:16:27 +0800

    000

--- C++大学教程练习题/第03章 类、对象和字符串介绍/3.13 Invoice/Invoice.h
+++ C++大学教程练习题/第03章 类、对象和字符串介绍/3.13 Invoice/Invoice.h
@@ -1,15 +1,23 @@
 #include <string>
+#include <iostream>
+#include <iomanip>
 
 class Invoice
 {
 public:
-    Invoice(std::string id, std::string describe, int sal, int prce) : ID(id), description(describe), sales(sal), price(prce) {}
+    Invoice(std::string id, std::string describe, int sal, int prce)
+        : ID("NULL"), description(describe), sales(sal), price(-1) 
+    {
+        this->ID = id;
+        // price = prce;
+    }
     void information()
     {
-        std::cout << "ID:\t" << ID << std::endl;
-        std::cout << "description:\t" << description << std::endl;
-        std::cout << "ID:\t" << ID << std::endl;
-        std::cout << "ID:\t" << ID << std::endl;
+        std::cout << std::left;
+        std::cout << std::setw(25) << "ID: " << ID << std::endl;
+        std::cout << std::setw(25) << "description: " << description << std::endl;
+        std::cout << std::setw(25) << "sales: " << sales << std::endl;
+        std::cout << std::setw(25) << "price: " << price << std::endl;
     }
 
 private:
--- C++大学教程练习题/第03章 类、对象和字符串介绍/3.13 Invoice/main_test.cpp
+++ C++大学教程练习题/第03章 类、对象和字符串介绍/3.13 Invoice/main_test.cpp
@@ -1,12 +1,22 @@
 #include <iostream>
 #include <string>
+#include <stdexcept>
 
 #include "Invoice.h"
 
 using namespace std;
 int main()
 {
-    Invoice invoice("TEST_ID","TEST_DESCRIPTION",5000,50);
+    Invoice invoice("TEST_ID", "TEST_DESCRIPTION", 5000, 50);
     invoice.information();
+
+    // int a = -1;
+    // if (a > 10)
+    // {
+    // }
+    // else
+    //     throw invalid_argument("fuck");
+    // cout << "return 0;";
+
     return 0;
 }
